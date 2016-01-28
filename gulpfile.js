440786ecb4f276c894e908ad863d0c189b12f447
Ryan Ricard <ryan.ricard@gmail.com>
2016-01-28 16:06:16 -0600

    Project setup
    - Add gulp
    - Add ESLint, configuration, and basic gulp linting task
    - Set dependent Node version

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/* eslint-disable no-process-exit */
+
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+
+const paths = {
+  scripts: ['!node_modules/**', './**/*.js'],
+  tests: ['./test/**/*-test.js']
+};
+
+gulp.task('lint', () => {
+  return gulp.src(paths.scripts)
+    .pipe(eslint())
+    .pipe(eslint.format());
+});
+
+gulp.task('lint:watch', ['lint'], () => {
+  return gulp.watch(paths.scripts, ['lint']);
+});
+
+gulp.task('default', ['lint']);
